a2b98a788edbe88988496cf6cb163de64d400623
David K <davecreate@yahoo.de>
2013-12-19 16:58:09 +0100

    Projekt angelegt ---- s

---
+++ disco-forum/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiscoForum::Application.config.secret_key_base = '247b0a8d0fcabcdf9e082ff3c2561abcd6edaeffebaad0c29c43e1f04bcb8c42b5efae19cc997aaf4f0697f3da1ec6a4a73aa6b4eb98da01dd3d2e9613b93d0a'
---
+++ disco-forum/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiscoForum::Application.config.session_store :cookie_store, key: '_disco-forum_session'
