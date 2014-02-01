d6c676ff2d2fea178b5423709a41d1a5ba6e26e0
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2014-02-01 20:30:14 +0100

    some fixes

--- app/projections/users_projection.rb
+++ app/projections/users_projection.rb
@@ -2,8 +2,10 @@ class UserProjection
   include ActiveProjection::ProjectionType
   
   def register_user_event(event)
-  	puts 'RAils - Projection 1'
-    User.create! event.values.merge(id: event.id)
+    puts 'RAils - Projection 1'
+    values = event.values.merge(id: event.id)
+    values.delete(:passwort)
+    User.create! values
     puts 'RAils - Projection'		
   end
 end
--- domain/projections/domain/users_projection.rb
+++ domain/projections/domain/users_projection.rb
@@ -3,8 +3,8 @@ module Domain
     include ActiveDomain::Projection
     
     def register_user_event(event)
-      User.create! event.values.merge(id: event.id)
-      puts 'Domain- Projection'		
+      User.create! id: event.id, benutzername: event.benutzername, passwort: event.passwort
+      puts 'Domain- Projection'
     end
 
   end
