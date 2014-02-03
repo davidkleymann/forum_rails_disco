11a1050956b2945ea9b84e479846ba2fa6e33b03
Julius <juwejuliwenz@gmx.de>
2014-02-03 18:19:52 +0100

    Sauber gemacht

--- app/projections/users_projection.rb
+++ app/projections/users_projection.rb
@@ -2,16 +2,8 @@ class UserProjection
   include ActiveProjection::ProjectionType
   
   def register_user_event(event)
-<<<<<<< HEAD
-	values = event.values.merge(id:event.id)
-	values.delete(:passwort)
-	User.create! values	
-=======
-    puts 'RAils - Projection 1'
-    values = event.values.merge(id: event.id)
-    values.delete(:passwort)
-    User.create! values
-    puts 'RAils - Projection'		
->>>>>>> d6c676ff2d2fea178b5423709a41d1a5ba6e26e0
+   values = event.values.merge(id:event.id)
+	 values.delete(:passwort)
+	 User.create! values	
   end
 end
