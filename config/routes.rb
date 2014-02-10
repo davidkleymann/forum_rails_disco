ab3e7544da007a16bf9d2a641db40d70f027b6fa
David K <davecreate@yahoo.de>
2014-02-10 18:03:04 +0100

    routes.rb repariert

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
-DiscoBlog::Application.routes.draw do
 
-  resources :admins
-
-  resources :themas
-
-  resources :adminmessages
-
-  post '/login' =>'users#login'
-
-
-  resources :users
-
-  get '/userpage' => 'users#userpage'
-
-
-  resources :topics 
-
-  resources :posts
-
-  get '/' =>'home#index'
-    get '/admins' =>'home#admins'
-
-  get 'event_stream' => 'event_source#stream'
-
-end
-=======
 DiscoBlog::Application.routes.draw do
 
   resources :themas
@@ -50,4 +23,4 @@ DiscoBlog::Application.routes.draw do
   get 'event_stream' => 'event_source#stream'
 
 end
->>>>>>> 908cadeefc9e3517573b840d757a638b03b4bc68
+
