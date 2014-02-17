4fbdf97de8aa7f97685dc72bff7d92686d41bcff
jwenzel <juwejuliwenz@gmx.de>
2014-02-17 09:43:57 +0100

    Admin aus Domain rausgenommen

--- domain/command_processors/domain/admin_processor.rb
+++
@@ -1,9 +0,0 @@
-module Domain
-  class AdminmessageProcessor
-    include ActiveDomain::CommandProcessor
-
-    process AuthenticateCommand do |command|
-    	command.is_valid_do {puts "erfolgreich"}
-    end
-  end
-end
--- domain/projections/domain/admins_projection.rb
+++
@@ -1,6 +0,0 @@
-module Domain
-  class AdminProjection
-    include ActiveDomain::Projection
-    
-  end
-end
