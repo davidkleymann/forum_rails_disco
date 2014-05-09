eb986e76af932b6c0f009f40e6db6d1f503b328b
jwenzel <juwejuliwenz@gmx.de>
2014-05-09 17:32:09 +0200

    Doku weitergearbeitet
    Posts aus der Domain genommen

--- app/models/last_post.rb
+++ app/models/last_post.rb
@@ -3,8 +3,6 @@ class LastPost < ActiveRecord::Base
   self.table_name = 'last_posts'
   belongs_to :user
   has_many :posts
-  
-  belongs_to :thema
 
   def self.for_user(user)
     where(user_id: user.id).order(created_at: :desc)
